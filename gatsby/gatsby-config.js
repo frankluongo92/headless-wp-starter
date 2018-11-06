5807a17591acb4bcdbb907398dd83f49e4f24337
Frank Luongo <frank.luongo@revzilla.com>
2018-11-06 14:20:35 -0500

    17: Getting Start with Gatsby & WordPress

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -25,6 +25,15 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'localhost:8080',
+        protocol: 'http',
+        hostingWPCOM: false,
+        useACF: true
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
